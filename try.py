0e48607e2f7623b0f8b1bb1a309188e5d374e145
Achyuth-desai <desai.achyutha2000@gmail.com>
2020-08-06 14:07:29 +0530

    Data collection working

---
+++ try.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+import time
+
+url = "https://www.nyse.com/ipo-center/recent-ipo"
+driver = webdriver.Firefox()
+driver.get(url)
+
+time.sleep(2)
+
+list_header =""
+data = ""
+content = driver.page_source
+soup = BeautifulSoup(content,'html.parser')
+driver.close()
+
+f= open("file.csv","w",encoding="utf-8")
+
+header = soup.find_all("table")[0].find('tr')
+
+for items in header : 
+	try:
+		list_header = list_header + items.get_text() + ","
+	except:
+		continue
+f.write(list_header)
+f.write("\n")
+
+HTML_data = soup.find_all("table")[0].find_all("tr")[1:]
+
+for tr in HTML_data:
+	td = tr.find_all("td")
+	for element in td:
+		try:
+			data = element.get_text().replace(",","|") + ","
+			f.write(data)
+		except:
+			continue
+	f.write("\n")
+f.close()
+#df = pd.DataFrame(data = data ,columns = list_header)
+#df.to_csv('data.csv')
